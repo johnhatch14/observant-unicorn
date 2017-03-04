9badc1abd70622e763c3c6051f97419925ab26f1
LoadingPleaseWait <michael@murphey.org>
2017-03-04 17:03:53 -0600

    Pedantic styling changes

--- sensor/sensor.py
+++ sensor/sensor.py
@@ -1,16 +1,21 @@
-from scapy.all import *
+#!/usr/bin/python3
+
+import scapy.all
 
 class UnicornSensor():
 
     IFACE = 'wlan1' # Interface we are capturing packets on, must be in monitor mode!!!
 
-    def main(self):
-        print("Hello World! We come in peace, bringing Soylent...")
-        sniff(iface = self.IFACE, prn = self.PacketHandler)
+    def capture(self):
+        scapy.all.sniff(iface = self.IFACE, prn = self.packetHandler)
 
-    def PacketHandler(self, pkt: Packet) :
+    def packetHandler(self, pkt: scapy.all.Packet) :
         # Prints all packets captured
-        print (pkt)
+        print(pkt)
+
+def main():
+    print("Hello World! We come in peace, bringing Soylent...")
+    UnicornSensor().capture()
 
 if __name__ == '__main__':
-    UnicornSensor().main()
+    main()
