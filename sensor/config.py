e0145a1f7a39538426f4a0f589783c1dcbc0acb8
LoadingPleaseWait <michael@murphey.org>
2017-03-05 04:13:19 -0600

    Read whitelist file, detect blacklisted MAC addresses, handle exceptions

--- sensor/config.py
+++ sensor/config.py
@@ -8,14 +8,17 @@ class Config():
 
     def loadConfig(self):
         # read config file
-        with open('config', 'r') as config_file:
-            self.interface=config_file.readline()
-            # remove new line
-            self.interface=remove_new_line(self.interface)
-            self.database_name=remove_new_line(config_file.readline())
-            self.database_host=remove_new_line(config_file.readline())
-            self.database_user=remove_new_line(config_file.readline())
-            self.databass_pass=remove_new_line(config_file.readline())
+        try:
+            with open('config', 'r') as config_file:
+                self.interface=config_file.readline()
+                # remove new line
+                self.interface=remove_new_line(self.interface)
+                self.database_name=remove_new_line(config_file.readline())
+                self.database_host=remove_new_line(config_file.readline())
+                self.database_user=remove_new_line(config_file.readline())
+                self.databass_pass=remove_new_line(config_file.readline())
+        except FileNotFoundError as error:
+            print("Please create '" + error.filename + "' file")
 
 def remove_new_line(string):
     return string[0:len(string)-1]
--- sensor/sensor.py
+++ sensor/sensor.py
@@ -6,22 +6,32 @@ from scapy.layers.dot11 import Dot11
 import time
 
 from config import Config
+from config import remove_new_line
 
 class UnicornSensor():
 
     ap_list = []
     devices = {}
     blacklist = []
+    whitelist = []
 
     def capture(self):
         configuration = Config()
         configuration.loadConfig()
-        # read blacklist
-        with open("blacklist", 'r') as blacklist_file:
-            mac = blacklist_file.readline()
-            while mac != '':
-                blacklist.append(mac)
+        # read blacklist and whitelist
+        try:
+            with open("blacklist", 'r') as blacklist_file:
                 mac = blacklist_file.readline()
+                while mac != '':
+                    self.blacklist.append(remove_new_line(mac))
+                    mac = blacklist_file.readline()
+            with open("whitelist", 'r') as whitelist_file:
+                mac = whitelist_file.readline()
+                while mac != "":
+                    self.whitelist.append(remove_new_line(mac))
+                    mac = whitelist_file.readline()
+        except FileNotFoundError as ex:
+            print("Please create '" + ex.filename + "' file")
         print("Listening on", configuration.interface)
         sniff(iface = configuration.interface, prn = self.packetHandler)
 
@@ -36,16 +46,22 @@ class UnicornSensor():
             if pkt_fields['type'] == 0 and pkt_fields['subtype'] == 8:  # Beacon
                 self.event_AccessPointHeard(pkt_fields['addr2'])
 
+            if pkt_fields['type'] == 0 and pkt_fields['subtype'] == (0 or 2): # Association Request
+                self.event_HeardAssociation(pkt_fields['addr1'], pkt_fields['addr2'])
+
     def event_HeardProbe(self, mac):
         print("HeardProbe from: " + str(mac))
         self.devices[str(mac)] = str(time.time())
-        if str(mac) in blacklist:
+        if str(mac) in self.blacklist:
             print("Blacklisted MAC address", str(mac), "detected!")
 
     def event_AccessPointHeard(self, mac):
         print("AccessPoint heard: " + str(mac))
-        if mac not in ap_list:
-            ap_list.append(mac)
+        if mac not in self.ap_list:
+            self.ap_list.append(mac)
+
+    def event_HeardAssociation(self, mac1, mac2):
+        print(str(mac2), "attempting to associate with", str(mac1))
 
 def main():
     print("Hello World! We come in peace, bringing Soylent...")
