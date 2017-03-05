14d19f845c41fb81d00fcac4459a293a6efa29e0
johnhatch14 <j_hatch01@verizon.net>
2017-03-05 00:53:16 -0600

    Recording timestamp

--- sensor/sensor.py
+++ sensor/sensor.py
@@ -5,10 +5,12 @@ from scapy.all import Packet
 from scapy.layers.dot11 import Dot11
 
 from config import Config
+import time
 
 class UnicornSensor():
 
     ap_list = []
+    devices = {}
 
     def capture(self):
         configuration = Config()
@@ -17,23 +19,25 @@ class UnicornSensor():
         sniff(iface = configuration.interface, prn = self.packetHandler)
 
     def packetHandler(self, rawpacket: Packet):
-        # Prints all packets captured
-        if rawpacket.haslayer(Dot11):
+
+        if rawpacket.haslayer(Dot11): # Make sure it is a dot11 packet
             pkt_fields = rawpacket.payload.fields
-            #print(pkt_fields)
-            if pkt_fields['type'] == 0 and pkt_fields['subtype'] == 4: # Probe Request
+
+            if pkt_fields['type'] == 0 and pkt_fields['subtype'] == 4:  # Probe Request
                 self.event_HeardProbe(pkt_fields['addr2'])
 
             if pkt_fields['type'] == 0 and pkt_fields['subtype'] == 8:  # Beacon
                 self.event_AccessPointHeard(pkt_fields['addr2'])
 
     def event_HeardProbe(self, mac):
-        print("HeardProbe from:" + str(mac))
+        print("HeardProbe from: " + str(mac))
+        self.devices[str(mac)] = str(time.time())
 
     def event_AccessPointHeard(self, mac):
-        print("AccessPoint heard:" + str(mac))
+        print("AccessPoint heard: " + str(mac))
 
 def main():
+    print("Hello World! We come in peace, bringing Soylent...")
     UnicornSensor().capture()
 
 if __name__ == '__main__':
